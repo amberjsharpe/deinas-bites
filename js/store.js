2773552925687e9c1060957eb93a4f3e5fbeb0b0
Amber Sharpe <amberjsharpe@gmail.com>
2018-04-04 18:11:48 -0500

    added cookie cards to store page

--- js/store.js
+++ js/store.js
@@ -1,31 +1,38 @@
 "use strict";
 
 let cookieStoreArray = [
-    "best-seller-images/jam_poppy.png",
-    "images/poppy_seeds.png",
-    "best-seller-images/oatmeal_bites.png",
-    "images/soft_butter.png",
-    "images/parm.png",
-    "best-seller-images/white_choc.png",
-    "images/jam_bites.png",
-    "images/choc_chips.png",
-    "images/nuts_caramel.png",
-    "images/butter.png",
-    "images/pecan_nuts.jpg",
-    "images/seeds.jpg"
+    {img:"best-seller-images/jam_poppy.png", title:"JAM & POPPY SEED BITES", price:"$1.00"},
+    {img:"images/poppy_seeds.png", title: "POPPY SEEDS BITES", price:"$2.00"},
+    {img:"best-seller-images/oatmeal_bites.png", title: "OATMEAL BITES", price:"$2.00"},
+    {img:"images/soft_butter.png", title: "SOFT BUTTER COOKIES", price:"$2.00"},
+    {img:"images/parm.png", title: "PARMESAN BITES", price:"$2.00"},
+    {img:"best-seller-images/white_choc.png", title: "WHITE CHOCOLATE BITES", price:"$1.00"},
+    {img:"images/jam_bites.png", title: "JAM BITES", price:"$1.00"},
+    {img:"images/choc_chips.png", title: "CHOCOLATE CHIPS", price:"$2.00"},
+    {img:"images/nuts_caramel.png", title: "NUTS & CARAMEL BITES", price:"$2.00"},
+    {img:"images/butter.png", title: "BUTTER COOKIES", price:"$1.00"},
+    {img:"images/pecan_nuts.jpg", title: "PECAN NUT BITES", price:"$2.00"},
+    {img:"images/seeds.jpg", title: "SEEDS BITES", price:"$2.00"}  
 ];
 
 
 
-let displayStoreArray = () => {
+let displayCookieStoreArray = (cookieStoreArray) => {
     for (var i = 0; i < cookieStoreArray.length; i++) {
         var cookieCard = `
-        <div class="cookieCard">
-            <img class="best-seller-img" src="${cookieStoreArray[i]}">
-            <h3>WHITE CHOCOLATE BITES</h3>
-            <p>$1.00</p>
+        <div class="cookieCard d-flex justify-content-center align-items-center flex-column">
+            <img class="best-seller-img" src="${cookieStoreArray[i].img}">
+            <h3>${cookieStoreArray[i].title}</h3>
+            <p>${cookieStoreArray[i].price}</p>
         </div>
-        `;
+        `; 
         document.querySelector('#store-display').innerHTML += cookieCard;
     }
+};
+
+displayCookieStoreArray(cookieStoreArray);
+
+module.exports = {
+    cookieStoreArray,
+    displayCookieStoreArray
 };
